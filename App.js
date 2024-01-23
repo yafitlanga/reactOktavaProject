a701d5e749969509ce724fc301294860d3e5d366
yafitlanga <69118199+yafitlanga@users.noreply.github.com>
2024-01-23 23:03:22 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,29 @@
+import "./App.css";
+import Users from "./component/Users";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./component/HomePage";
+import About from "./component/About";
+import Concerts from "./component/Concerts";
+import ContactUs from "./component/ContactUs";
+import LogInRegister from "./component/LogInRegister";
+import HomePageManager from "./component/HomePageManager";
+import AllTeachers from "./component/AllTeachers";
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<HomePage />}>
+          <Route path="about" element={<About />}></Route>
+          <Route path="contactUs" element={<ContactUs />}></Route>
+          <Route path="logInRegister" element={<LogInRegister />}></Route>
+          <Route path="concerts" element={<Concerts />}></Route>
+          <Route path="homePageManager" element={<HomePageManager />}></Route>
+          <Route path="allTeachers" element={<AllTeachers />}></Route>
+        </Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
